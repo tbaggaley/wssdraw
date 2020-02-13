ea627c184fd5d97818fcceaac239d425203e0273
Thomas Baggaley <baggaleyto@cardiff.ac.uk>
2020-02-13 12:19:36 +0000

    Move back to supervisor-start of broadcaster, implement dynamic IP in clientside - fix

--- apps/ws_server/src/ws_server_sup.erl
+++ apps/ws_server/src/ws_server_sup.erl
@@ -29,9 +29,9 @@ init([]) ->
     SupFlags = #{strategy => one_for_all,
                  intensity => 0,
                  period => 1},
-    %%ChildSpecs = [#{id => broadcaster_child,
-     %%               start => {broadcaster, start_link, []}}],
-    ChildSpecs = [],
+    ChildSpecs = [#{id => broadcaster_child,
+                   start => {broadcaster, start_link, []}}],
+    
     {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
